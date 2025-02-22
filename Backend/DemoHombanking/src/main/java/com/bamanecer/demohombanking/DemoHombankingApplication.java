871518e7bc31e5bc58c9c2413fb67da1354135b0
Beatriz Ebert <ebert.beatriz.be@gmail.com>
2025-02-22 14:52:45 -0300

    inicio de back

---
+++ Backend/DemoHombanking/src/main/java/com/bamanecer/demohombanking/DemoHombankingApplication.java
@@ -0,0 +1,13 @@
+package com.bamanecer.demohombanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoHombankingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoHombankingApplication.class, args);
+    }
+
+}
