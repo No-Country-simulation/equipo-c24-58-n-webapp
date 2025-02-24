622574546900da2c94e52beef46fe0011ed61bf5
Alejandro <alejodev2022@gmail.com>
2025-02-23 22:07:56 -0500

    Actualizar el componente Header para incluir íconos de usuario y notificaciones, y ajustar el color de fondo

--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom";
-import { useAuth } from "../hooks/useAuth";
-
+import { useAuth } from "../contexts/AuthContext";
 import {  User } from "lucide-react";
 import NotificationIcon from "./NotificationIcon";
 
