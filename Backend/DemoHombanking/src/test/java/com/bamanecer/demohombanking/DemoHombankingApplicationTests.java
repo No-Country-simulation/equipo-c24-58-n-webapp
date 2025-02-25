c854005a8a35a5e83b3da6fef56c3ac33fc3bd44
antonio clavijo <clavijo1785@gmail.com>
2025-02-24 22:08:05 -0500

    se crea una nueva rama

---
+++ Backend/DemoHombanking/src/test/java/com/bamanecer/demohombanking/DemoHombankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bamanecer.demohombanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoHombankingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
